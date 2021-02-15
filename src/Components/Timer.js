e53bd5ce97165c050eeaacf27a7a8e114aaf3a3f
mccloudt <tomjmccloud@gmail.com>
2021-02-15 00:17:32 +0000

    completed own version of pomodoro timer for FCC

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -8,36 +8,75 @@ class Timer extends Component {
             timerTitle: `Timer Number ${this.props.tKey}`,
             inputDisplay: false,
             backColor: '',
-            breakTime: 5,
+            breakTime: 10,
             workTime: 25,
+            timerSeconds: 0,
             phase: "Work",
             paused: false,
-            stopped: false,
+            timerRunning: false,
         }
 
         this.editTitle = this.editTitle.bind(this);
         this.updateTimerTitle = this.updateTimerTitle.bind(this);
         this.modifyTime = this.modifyTime.bind(this);
+        this.prettyTime = this.prettyTime.bind(this);
+        this.runTimer = this.runTimer.bind(this);
+        this.initialTime = this.initialTime.bind(this);
+        this.setPaused = this.setPaused.bind(this);
     }
 
 
     componentDidMount(){
         const colors = ["red", "yellow", "green"];
         this.setState({backColor: colors[Math.floor(Math.random() * colors.length)]});
+        this.initialTime();
+        console.log(this.state.timerSeconds);
+    }
+
+    initialTime(){
+        if(this.state.phase == "Work"){
+            this.setState({timerSeconds: this.state.workTime * 60});
+        }else{
+            this.setState({timerSeconds: this.state.breakTime * 60});
+        }
     }
 
     modifyTime(timer, direction){
-        console.log(timer, direction);
         switch(timer){
             case "break":
-                direction == "up" ? this.setState({breakTime: this.state.breakTime + 1}) : this.state.breakTime > 1 && this.setState({breakTime: this.state.breakTime - 1})
+                if(direction == "up"){
+                    if(this.state.phase == "Break"){
+                        this.setState({breakTime: this.state.breakTime + 1, timerSeconds: (this.state.breakTime + 1) * 60})
+                    }else{
+                        this.setState({breakTime: this.state.breakTime + 1});
+                    }
+                }else{
+                    if(this.state.phase == "Break"){
+                        this.state.breakTime > 1 && this.setState({breakTime: this.state.breakTime - 1, timerSeconds: (this.state.breakTime - 1) * 60})
+                    }else{
+                        this.setState({breakTime: this.state.breakTime - 1});
+                    }
+                }
+            case "work": 
+                if(direction == "up"){
+                    if(this.state.phase == "Work"){
+                        this.setState({workTime: this.state.workTime + 1, timerSeconds: (this.state.workTime + 1) * 60})
+                    }else{
+                        this.setState({workTime: this.state.workTime + 1});
+                    }
+                }else{
+                    if(this.state.phase == "Work"){
+                        this.state.workTime > 1 && this.setState({workTime: this.state.workTime - 1, timerSeconds: (this.state.workTime - 1) * 60})
+                    }else{
+                        this.setState({workTime: this.state.workTime - 1});
+                    }
+                }
+                break;
         }
     }
 
     updateTimerTitle(e){
-
         if(this.state.inputDisplay){
-            console.log(e.target.value);
             this.setState({timerTitle: e.target.value});
         }
     }
@@ -46,8 +85,49 @@ class Timer extends Component {
         this.setState({inputDisplay: !this.state.inputDisplay});
     }
 
-    render(){ 
+    setPaused(){
+        this.setState({paused: !this.state.paused});
+    }
 
+    runTimer(){
+        let timer = ''
+        if(!this.state.timerRunning){
+            this.setState({timerRunning: true})
+             timer = setInterval(() => {
+                !this.state.paused && this.setState({timerSeconds: this.state.timerSeconds - 1})
+                if(this.state.timerSeconds == 0){
+                    this.setState({timerRunning: false})
+                    if(this.state.phase == "Work"){
+                        this.setState({phase: "Break"});
+                        this.initialTime();
+                    }else{
+                        this.setState({phase: "Work"});
+                        this.initialTime();
+                    }
+                    clearInterval(timer);
+                }
+            }, 1000)
+        }
+        else{
+            clearInterval(timer);
+        }
+
+        
+    }
+
+    prettyTime(){
+        let minutes = 0;
+        let seconds = 0;
+        let placeHolderSeconds = this.state.timerSeconds;
+        minutes = Math.floor( placeHolderSeconds / 60)
+        seconds = placeHolderSeconds - (minutes * 60);
+        return `${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ''}${seconds}`;
+
+
+                
+    }
+
+    render(){ 
         return (
             <div className="timer">
                 <div className={`title-container background-${this.state.backColor}`}>
@@ -91,9 +171,19 @@ class Timer extends Component {
                             }}>👇</span>
                         </div>
                     </div>
-                    <div>
-                        <h1>{this.state.phase}:</h1>
+                    <div id="current-phase">
+                        <h3>{this.state.phase}</h3>
                     </div>
+                    <div id="time">
+                        <p>{this.prettyTime()}</p>
+                    </div>
+                    <button id="timerButton" onClick={this.state.timerRunning ? this.setPaused : this.runTimer}>{
+                    this.state.timerRunning 
+                    ?  this.state.paused ? "Resume" :
+                    "Pause"
+                    : "Start"}
+                    
+                    </button>
                 </div>
             </div>
         )
